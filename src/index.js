11456e2d8d32e366f46a1954e2a4f22729621621
sonyohann <93103346+sonyohann@users.noreply.github.com>
2021-11-23 16:21:38 +0900

    Reduce

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-const array = [1, 2, 3, 4, 5];
-console.log(array.join());
-console.log(array.join(' '));
-console.log(array.join(', '));
+const numbers = [1, 2, 3, 4, 5];
+
+const avg = numbers.reduce((accumulator, current, index, array) => {
+  if (index === array.length - 1) {
+    return (accumulator + current) / array.length;
+  }
+  return accumulator + current;
+}, 0);
+console.log(avg);
