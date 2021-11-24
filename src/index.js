4ccd964e9416ddeb582ded36c32bc240d4138e3e
sonyohann <93103346+sonyohann@users.noreply.github.com>
2021-11-24 10:16:39 +0900

    Class 2

--- src/index.js
+++ src/index.js
@@ -1,30 +1,24 @@
-class Animal {
-  constructor(type, name, sound) {
-    this.type = type;
+class Food {
+  constructor(name) {
     this.name = name;
-    this.sound = sound;
+    this.brands = [];
   }
-  say() {
-    console.log(this.sound);
+  addBrand(brand) {
+    this.brands.push(brand);
   }
-}
-
-class Dog extends Animal {
-  constructor(name, sound) {
-    super('개', name, sound);
+  print() {
+    console.log(`${this.name}을/를 파는 음식점들`);
+    console.log(this.brands.join(', '));
   }
 }
 
-class Cat extends Animal {
-  constructor(name, sound) {
-    super('고양이', name, sound);
-  }
-}
+const pizza = new Food('피자');
+pizza.addBrand('피자헛');
+pizza.addBrand('도미노 피자');
 
-const dog = new Dog('멍멍이', '멍멍');
-const cat = new Cat('고양이', '야옹');
-const cat2 = new Cat('야오오오옹이', '야오오옹');
+const chicken = new Food('치킨');
+chicken.addBrand('굽네치킨');
+chicken.addBrand('BBQ');
 
-dog.say();
-cat.say();
-cat2.say();
+pizza.print();
+chicken.print();
